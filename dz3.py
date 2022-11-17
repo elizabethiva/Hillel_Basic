f70246854f4b6a7309210fc7992c1517e262fb5c
Yelizaveta <lizavetamania@gmail.com>
2022-11-17 21:43:39 +0200

    without print1

--- dz3.py
+++ dz3.py
@@ -4,4 +4,4 @@ second = x // 1000 % 10
 third = x % 1000 // 100
 fourth = x % 100 // 10
 fifth = x % 1000 % 10
-print(fifth * 10000 + fourth * 1000 + third * 100 + second * 10 + first * 1)
+print(fifth * 1000 + fourth * 1000 + third * 100 + second * 10 + first * 1)
