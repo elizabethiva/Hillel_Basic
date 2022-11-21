181714df9ceab2dc27a2cd7809819ffea42c2ac8
Yelizaveta <lizavetamania@gmail.com>
2022-11-21 20:39:32 +0200

    dz5

--- hometasks/dz5.py
+++ hometasks/dz5.py
@@ -1,8 +1,12 @@
-data = input('Type list: ')
-lst = list(data)
-if len(lst) == 0 or 1:
-    print(lst)
-else:
-    lst.insert(0, lst[-1])
-    del(lst[-1])
-    print(lst)
+lst = [1, 2, 3, 4, 5, 6]
+middle = len(lst) // 2
+first_half = lst[:middle]
+second_half = lst[middle:]
+print(first_half, second_half)
+lst2 = [1, 2, 3]
+lst2_edit = lst.copy()
+del(lst2_edit[0])
+middle2 = len(lst2_edit) // 2
+first_half2 = lst2_edit[lst2[0], :middle2]
+second_half2 = lst2_edit[middle2:]
+print(first_half2, second_half2)
