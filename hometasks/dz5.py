8fda0eba5750aea696a5e93244c6f5ff14b04b89
Yelizaveta <lizavetamania@gmail.com>
2022-11-21 16:54:45 +0200

    dz5_2

--- hometasks/dz5.py
+++ hometasks/dz5.py
@@ -1,4 +1,8 @@
-first_list = [2, 4, 7, 11, 0, -2, 8]
-first_list.insert(0, first_list[-1])
-del(first_list[-1])
-print(first_list)
+data = input('Type list: ')
+lst = list(data)
+if len(lst) == 0 or 1:
+    print(lst)
+else:
+    lst.insert(0, lst[-1])
+    del(lst[-1])
+    print(lst)
