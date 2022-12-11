95b129e1b46429595b506f6e7e3aefdcf20266df
Yelizaveta <lizavetamania@gmail.com>
2022-12-11 20:34:09 +0200

    dz19_2

--- hometasks/dz19.py
+++ hometasks/dz19.py
@@ -1,20 +1,19 @@
-#Напишите фукцию common_elements, которая сгенерирует два списка с разным количеством элементов (кол-во задается параметром функции).
-# Один список с числами кратными 3, другой с числами кратными 5.
-# С помощью множеств создайте набор с числами, которые есть в обоих множествах (пересечение).
-# Функция должна вернуть это множество в качестве результата своей работы.
 import random
-def common_elements(a, b, c, d):
-    first_list = [random.randint(1, 100) for i in range(random.randint(a, b))]
-    x = set()
-    for i in first_list:
-        if i % 3 == 0:
-            x.add(i)
-    second_list = [random.randint(1, 100) for i in range(random.randint(c, d))]
-    y = set()
-    for a in second_list:
-        if a % 5 == 0:
-            y.add(a)
-    z = x.intersection(y)
-    return z
-d = common_elements(1, 21, 5, 25)
-print(d)
+
+def common_elements(size_1, size_2):
+    first_list = []
+    while len(first_list) != size_1:
+        x = random.randint(0, 100)
+        if x % 3 == 0:
+            first_list.append(x)
+    first_set = set(first_list)
+    second_list = []
+    while len(second_list) != size_2:
+        x = random.randint(0, 100)
+        if x % 5 == 0:
+            second_list.append(x)
+    second_set = set(second_list)
+    intersection = first_set.intersection(second_list)
+    return intersection
+intersection = common_elements(100, 100)
+print(intersection)
